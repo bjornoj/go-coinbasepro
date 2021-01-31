4bfbcc93d0c9daad4662567f841390891ec48a70
jalborg <bo@jalborg.com>
2021-01-31 18:21:34 +0100

    add withdrawal fiat with withdrawals/payment-method endpoint

--- withdrawals.go
+++ withdrawals.go
@@ -16,6 +16,17 @@ type WithdrawalCoinbase struct {
 	CoinbaseAccountID string `json:"coinbase_account_id"`
 }
 
+type WithdrawalFiatRequest struct {
+	Currency        string `json:"currency"`
+	Amount          string `json:"amount"`
+	PaymentMethodID string `json:"payment_method_id"`
+}
+
+type WithdrawalFiatResponse struct {
+	ID string `json:"id"`
+	WithdrawalFiatRequest
+}
+
 func (c *Client) CreateWithdrawalCrypto(newWithdrawalCrypto *WithdrawalCrypto) (WithdrawalCrypto, error) {
 	var savedWithdrawal WithdrawalCrypto
 	url := fmt.Sprintf("/withdrawals/crypto")
@@ -27,5 +38,12 @@ func (c *Client) CreateWithdrawalCoinbase(newWithdrawalCoinbase *WithdrawalCoinb
 	var savedWithdrawal WithdrawalCoinbase
 	url := fmt.Sprintf("/withdrawals/coinbase-account")
 	_, err := c.Request("POST", url, newWithdrawalCoinbase, &savedWithdrawal)
+	return savedWithdrawal, err
+}
+
+func (c *Client) CreateWithdrawalFiat(newWithdrawalFiat *WithdrawalFiatRequest) (WithdrawalFiatResponse, error) {
+	var savedWithdrawal WithdrawalFiatResponse
+	url := fmt.Sprintf("/withdrawals/payment-method")
+	_, err := c.Request("POST", url, newWithdrawalFiat, &savedWithdrawal)
 	return savedWithdrawal, err
 }
