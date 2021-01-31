ac251572c0e6a3a3fff67544f7cb5bb2fd8b8fc9
jalborg <bo@jalborg.com>
2021-01-31 18:46:51 +0100

    list withdrawals and deposits

--- transfer.go
+++ transfer.go
@@ -1,6 +1,7 @@
 package coinbasepro
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -10,10 +11,59 @@ type Transfer struct {
 	CoinbaseAccountID string `json:"coinbase_account_id,string"`
 }
 
+type ListTransferRequest struct {
+	// Type can be deposit or withdraw. Required
+	Type string `json:"type"`
+	// (Optional) Limit list of transfers to this profile_id. By default, it retrieves transfers across all of the user's profiles
+	ProfileID string `json:"profile_id"`
+
+	PaginationParams
+}
+
+type ListTransferResponse struct {}
+
 func (c *Client) CreateTransfer(newTransfer *Transfer) (Transfer, error) {
 	var savedTransfer Transfer
 
 	url := fmt.Sprintf("/transfers")
 	_, err := c.Request("POST", url, newTransfer, &savedTransfer)
 	return savedTransfer, err
+}
+
+func (c *Client) ListTransfers(request ListTransferRequest) ([]Transfer, error) {
+	var savedTransfers []Transfer
+
+	if request.Type == "" {
+		return nil, errors.New("type is required")
+	}
+
+	url := fmt.Sprintf("/transfers")
+	_, err := c.Request("GET", url, request, &savedTransfers)
+	return savedTransfers, err
+}
+
+func (c *Client) ListWithdrawals(profileID string, pagination PaginationParams) ([]Transfer, error) {
+	var savedTransfers []Transfer
+	request := ListTransferRequest{
+		Type:             "withdraw",
+		ProfileID:        profileID,
+		PaginationParams: pagination,
+	}
+
+	url := fmt.Sprintf("/transfers")
+	_, err := c.Request("GET", url, request, &savedTransfers)
+	return savedTransfers, err
+}
+
+func (c *Client) ListDeposits(profileID string, pagination PaginationParams) ([]Transfer, error) {
+	var savedTransfers []Transfer
+	request := ListTransferRequest{
+		Type:             "deposit",
+		ProfileID:        profileID,
+		PaginationParams: pagination,
+	}
+
+	url := fmt.Sprintf("/transfers")
+	_, err := c.Request("GET", url, request, &savedTransfers)
+	return savedTransfers, err
 }
